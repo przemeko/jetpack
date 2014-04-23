7d4c1d01694017aa9ad4755a0d817a11d3af76d2
Przemyslaw Kocznur <pkocznur@mordor.local.opcom.pl>
2014-04-23 12:41:40 +0200

    added spring force

---
+++ src/forces/spring.js
@@ -0,0 +1,48 @@
+var jetpack = jetpack || {};
+jetpack.forces = jetpack.forces || {};
+
+/**
+ * Spring force
+ * 
+ * @param {Particle} otherParticle
+ * @param {Number} springConstant
+ * @param {Number} restLength
+ * @returns {Spring} this
+ */
+jetpack.forces.Spring = function(otherParticle, springConstant, restLength) {
+    this.otherParticle = otherParticle || new jetpack.Particle();
+    this.springConstant = springConstant || 0; // k
+    this.restLength = restLength || 0; // r0
+    
+    this.vector = new jetpack.Vector(); // d
+    this.magnitude = 0;
+};
+
+/**
+ * Hook's law
+ * F = -k(r - r0)  // r0 - rest length
+ * in 3d:
+ * F =  -k(|d| - r0)dn // dn - normalize d
+ * 
+ * @param {Particle} particle
+ * @param {Number} duration
+ * @returns {Vector} force
+ */
+jetpack.forces.Spring.prototype.calculateForce = function(particle, duration) {
+
+    // Calculate the vector of the spring - d
+    this.vector.clear();
+    this.vector.add(particle.getPosition());
+    this.vector.sub(this.otherParticle.getPosition());
+
+    // Calculate the magnitude of the force 
+    this.magnitude = Math.abs(this.vector.magnitude() - this.restLength); // |d| - r0
+
+    // Calculate the final force and apply it
+    this.vector.normalize(); // dn
+    this.vector.scale(-this.springConstant*this.magnitude);
+    
+    return this.vector;
+};
+
+
